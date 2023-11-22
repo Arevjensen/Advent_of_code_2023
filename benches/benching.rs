d32b4438f4a56f00fed21d945600d8ce912e8828
Are Velde Jensen <are.velde.jensen@nrk.no>
2023-11-22 17:11:27 +0100

    renaming stuff

--- benches/benching.rs
+++ benches/benching.rs
@@ -1,17 +1,14 @@
-use aoc_2023::day1::{self};
+use aoc_2023::day01::{self};
 use aoc_2023::helpers;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    const DAY: &str = "1";
-    const PART: &str = "1";
-
     c.bench_function("day1 part 1 load file", |b| {
-        b.iter(|| day1::part1(black_box(&helpers::loader::read_text_from_file(PART, DAY))));
+        b.iter(|| day01::part1(black_box(&helpers::loader::read_text_from_file("1", "1"))));
     });
 
     c.bench_function("day1 part 1 include str", |b| {
-        b.iter(|| day1::part1(black_box(include_str!("../input/day_1_1.txt"))));
+        b.iter(|| day01::part1(black_box(include_str!("../input/day_1_1.txt"))));
     });
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-pub mod day1;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
+pub mod day08;
+pub mod day09;
 pub mod day10;
 pub mod day11;
 pub mod day12;
@@ -9,33 +17,25 @@ pub mod day16;
 pub mod day17;
 pub mod day18;
 pub mod day19;
-pub mod day2;
 pub mod day20;
 pub mod day21;
 pub mod day22;
 pub mod day23;
 pub mod day24;
 pub mod day25;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-pub mod day8;
-pub mod day9;
 pub mod helpers;
 
 pub fn run(day: u8, part: &str) {
     match day {
-        1 => day1::run(part),
-        2 => day2::run(part),
-        3 => day3::run(part),
-        4 => day4::run(part),
-        5 => day5::run(part),
-        6 => day6::run(part),
-        7 => day7::run(part),
-        8 => day8::run(part),
-        9 => day9::run(part),
+        1 => day01::run(part),
+        2 => day02::run(part),
+        3 => day03::run(part),
+        4 => day04::run(part),
+        5 => day05::run(part),
+        6 => day06::run(part),
+        7 => day07::run(part),
+        8 => day08::run(part),
+        9 => day09::run(part),
         10 => day10::run(part),
         11 => day11::run(part),
         12 => day12::run(part),
