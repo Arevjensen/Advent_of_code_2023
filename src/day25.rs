299b1781cae3d0cbee6f5dd9c2848e26794a2d06
Are Velde Jensen <are.velde.jensen@nrk.no>
2023-11-22 14:54:57 +0100

    added all days

---
+++ src/day25.rs
@@ -0,0 +1,44 @@
+use crate::helpers::loader;
+use crate::helpers::Solution;
+
+const DAY: &str = "25";
+
+pub fn run(part: &str) {
+    let result = match part {
+        "1" => part1(loader::read_text_from_file(part, DAY).as_str()),
+        "2" => part2(loader::read_text_from_file(part, DAY).as_str()),
+        _ => unimplemented!(),
+    };
+    println!("Solution for part {} on day {} is:", part, DAY);
+    println!("{:?}", result)
+}
+
+fn part1(input: &str) -> Solution {
+    Solution::from(100)
+}
+
+fn part2(input: &str) -> Solution {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT_ONE: &str = r"";
+    const TEST_INPUT_TWO: &str = r"";
+
+    #[test]
+    fn test_part_1() {
+        let fasit = Solution::from(100);
+        let part_solution = part1(TEST_INPUT_ONE);
+        assert_eq!(fasit, part_solution);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let fasit = Solution::from(200);
+        let my_soultion = part2(TEST_INPUT_TWO);
+        assert_eq!(fasit, my_soultion);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 use clap::Parser;
 mod day1;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day2;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
 mod helpers;
 
 #[derive(Parser, Debug)]
@@ -18,29 +42,30 @@ fn main() {
 
     match args.day {
         1 => day1::run(args.part.as_str()),
-        2 => todo!(),
-        3 => todo!(),
-        4 => todo!(),
-        5 => todo!(),
-        6 => todo!(),
-        7 => todo!(),
-        8 => todo!(),
-        9 => todo!(),
-        10 => todo!(),
-        11 => todo!(),
-        12 => todo!(),
-        13 => todo!(),
-        14 => todo!(),
-        15 => todo!(),
-        16 => todo!(),
-        17 => todo!(),
-        18 => todo!(),
-        19 => todo!(),
-        20 => todo!(),
-        21 => todo!(),
-        22 => todo!(),
-        23 => todo!(),
-        24 => todo!(),
-        _ => unimplemented!("Only days between 1-24"),
+        2 => day2::run(args.part.as_str()),
+        3 => day3::run(args.part.as_str()),
+        4 => day4::run(args.part.as_str()),
+        5 => day5::run(args.part.as_str()),
+        6 => day6::run(args.part.as_str()),
+        7 => day7::run(args.part.as_str()),
+        8 => day8::run(args.part.as_str()),
+        9 => day9::run(args.part.as_str()),
+        10 => day10::run(args.part.as_str()),
+        11 => day11::run(args.part.as_str()),
+        12 => day12::run(args.part.as_str()),
+        13 => day13::run(args.part.as_str()),
+        14 => day14::run(args.part.as_str()),
+        15 => day15::run(args.part.as_str()),
+        16 => day16::run(args.part.as_str()),
+        17 => day17::run(args.part.as_str()),
+        18 => day18::run(args.part.as_str()),
+        19 => day19::run(args.part.as_str()),
+        20 => day20::run(args.part.as_str()),
+        21 => day21::run(args.part.as_str()),
+        22 => day22::run(args.part.as_str()),
+        23 => day23::run(args.part.as_str()),
+        24 => day24::run(args.part.as_str()),
+        25 => day25::run(args.part.as_str()),
+        _ => unimplemented!("Only days between 1-25"),
     }
 }
