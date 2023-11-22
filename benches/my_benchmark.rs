66db2b2372bb7448d79215c713193908194f41fd
Are Velde Jensen <are.velde.jensen@nrk.no>
2023-11-22 15:49:37 +0100

    real file loading

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,13 +1,14 @@
 use aoc_2023::day1::{self};
+use aoc_2023::helpers;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("day1 part 1", |b| {
-        b.iter(|| day1::part1(black_box(include_str!("../input/day_1_1.txt"))));
+        b.iter(|| day1::part1(black_box(&helpers::loader::read_text_from_file("1", "1"))));
     });
 
     c.bench_function("day1 part 2", |b| {
-        b.iter(|| day1::part2(black_box(include_str!("../input/day_1_1.txt"))));
+        b.iter(|| day1::part2(black_box(&helpers::loader::read_text_from_file("1", "1"))));
     });
 }
 
