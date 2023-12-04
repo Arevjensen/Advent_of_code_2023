4f2591c3ab8f28fef2706f3630984d0840b78780
Are Velde Jensen <are.velde.jensen@nrk.no>
2023-12-04 16:35:04 +0100

    day3 part 2 setup wip

--- src/day03.rs
+++ src/day03.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
-use std::fmt::DebugTuple;
+use std::collections::{HashMap, HashSet};
 
 use crate::helpers::loader;
 use crate::helpers::solution::Solution;
@@ -123,131 +122,64 @@ fn check_neigtbours_part_1(line: usize, x: usize, check_vec: &Vec<Vec<char>>) ->
     false
 }
 
-fn check_next_to_star_part_2(
-    line: usize,
-    x: usize,
-    check_vec: &Vec<Vec<char>>,
-) -> (bool, HashMap<(u32, u32), u32>) {
-    let mut next_to_symbol = false;
-    let mut star_coordinates: HashMap<(u32, u32), u32> = HashMap::new();
-    if line != 0 {
-        if let Some(line_above) = check_vec.get(line - 1) {
-            if x != 0 {
-                if let Some(up_l) = line_above.get(x - 1) {
-                    match *up_l == '*' {
-                        true => {
-                            next_to_symbol = true;
-                            match star_coordinates.get_mut(&(line as u32, x as u32 - 1)) {
-                                Some(update) => *update += 1,
-                                None => (),
-                            }
-                        }
-                        false => (),
-                    }
-                }
-            }
-            if let Some(up) = line_above.get(x) {
-                match *up != '.' && !up.is_digit(10) {
-                    true => next_to_symbol = true,
-                    false => (),
-                }
-            }
-            if let Some(up_r) = line_above.get(x + 1) {
-                match *up_r != '.' && !up_r.is_digit(10) {
-                    true => next_to_symbol = true,
-                    false => (),
-                }
-            }
-        }
-    }
-
-    if let Some(line_below) = check_vec.get(line + 1) {
-        if x != 0 {
-            if let Some(down_l) = line_below.get(x - 1) {
-                match *down_l != '.' && !down_l.is_digit(10) {
-                    true => next_to_symbol = true,
-                    false => (),
-                }
-            }
-        }
-        if let Some(down) = line_below.get(x) {
-            match *down != '.' && !down.is_digit(10) {
-                true => next_to_symbol = true,
-                false => (),
-            }
-        }
-        if let Some(down_r) = line_below.get(x + 1) {
-            match *down_r != '.' && !down_r.is_digit(10) {
-                true => next_to_symbol = true,
-                false => (),
-            }
-        }
-    }
-    if let Some(line_same) = check_vec.get(line) {
-        if x != 0 {
-            if let Some(same_l) = line_same.get(x - 1) {
-                match *same_l != '.' && !same_l.is_digit(10) {
-                    true => next_to_symbol = true,
-                    false => (),
-                }
-            }
-        }
-        if let Some(same_r) = line_same.get(x + 1) {
-            match *same_r != '.' && !same_r.is_digit(10) {
-                true => next_to_symbol = true,
-                false => (),
-            }
-        }
-    }
-    (next_to_symbol, star_coordinates)
-}
 pub fn part2(input: &str) -> Solution {
     let schematic_grid = input
         .lines()
         .map(|x| x.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
-    // let possible_gears = schematic_grid
-    //     .iter()
-    //     .enumerate()
-    //     .map(|(line_nmbr, x)| {
-    //         (
-    //             line_nmbr,
-    //             x.iter()
-    //                 .enumerate()
-    //                 .filter(|(_, symbol)| **symbol == '*')
-    //                 .collect::<Vec<(usize, &char)>>(),
-    //         )
-    //     })
-    //     .collect::<Vec<(usize, Vec<(usize, &char)>)>>();
+    let gear_coordinates = schematic_grid
+        .iter()
+        .enumerate()
+        .map(|(y, x)| {
+            x.iter()
+                .enumerate()
+                .filter(|(x, c)| **c == '*')
+                .map(move |(x, c)| (y as isize, x as isize))
+        })
+        .flatten()
+        .collect::<Vec<(isize, isize)>>();
 
-    // dbg!(possible_gears);
+    let mut number_string = String::new();
+    let mut number_coords: Vec<(isize, isize)> = Vec::new();
+    let mut number_and_coords_vec = Vec::new();
+
+    let number_coordinates: Vec<(Vec<(isize, isize)>, u32)> = schematic_grid
+        .iter()
+        .enumerate()
+        .map(|(y, c)| {
+            number_and_coords_vec.clear();
+            //chars i line
+            c.iter().enumerate().for_each(|(x, c)| {
+                if x == 0 && number_string.len() != 0 {
+                    number_and_coords_vec
+                        .push((number_coords.clone(), number_string.parse::<u32>().unwrap()));
+                    number_string = "".to_string();
+                    number_coords.clear();
+                }
+                if c.is_digit(10) {
+                    number_string.push(*c);
+                    number_coords.push((y as isize, x as isize));
+                } else {
+                    if number_string.len() != 0 {
+                        number_and_coords_vec
+                            .push((number_coords.clone(), number_string.parse::<u32>().unwrap()));
+                    }
+                    number_string = "".to_string();
+                    number_coords.clear();
+                }
+            });
+            number_and_coords_vec.clone()
+        })
+        .flatten()
+        .collect();
+
+    dbg!(&number_coordinates);
+    // .flatten()
+    // .collect();
+    // dbg!(number_and_coords_vec);
 
     todo!()
-}
-
-fn part_2_gear_exactly_two_adjacent(
-    line_idx: usize,
-    char_idx: usize,
-    schematic: Vec<Vec<char>>,
-) -> usize {
-    let mut number_of_unique_adjacent = 0;
-    if line_idx != 0 {
-        if let Some(line_above) = schematic.get(line_idx - 1) {}
-    }
-    if let Some(line_below) = schematic.get(line_idx + 1) {}
-
-    let rigth_side = match schematic.get(line_idx).unwrap().get(char_idx - 1) {
-        Some(char_value) => {
-            if char_value.is_digit(10) {
-                number_of_unique_adjacent += 1
-            }
-        }
-        None => (),
-    };
-    let left_side = 0;
-
-    number_of_unique_adjacent
 }
 
 #[cfg(test)]
