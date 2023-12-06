11b222efae50d1721338e76d68374b8f10277296
Are Velde Jensen <are.velde.jensen@nrk.no>
2023-12-06 08:52:39 +0100

    dag 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,3 +1,5 @@
+use std::iter::zip;
+
 use crate::helpers::loader;
 use crate::helpers::solution::Solution;
 
@@ -14,30 +16,97 @@ pub fn run(part: &str) {
 }
 
 pub fn part1(input: &str) -> Solution {
-    Solution::from(100)
+    let (time_str, distance_str) = input.split_once("\n").unwrap();
+    let times = time_str
+        .split_once(": ")
+        .unwrap()
+        .1
+        .trim()
+        .split_whitespace()
+        .filter_map(|x| x.trim().parse::<usize>().ok());
+
+    let distances = distance_str
+        .split_once(": ")
+        .unwrap()
+        .1
+        .trim()
+        .split_whitespace()
+        .filter_map(|x| x.trim().parse::<usize>().ok());
+
+    let races = zip(times, distances)
+        .map(|(time, distance)| (time, distance))
+        .collect::<Vec<(usize, usize)>>();
+
+    let mut sum_result_vec: Vec<usize> = Vec::new();
+
+    for (race_time, race_record) in races {
+        let mut speeds_faster_than_record = 0;
+        for speed_held in 0..race_time {
+            let distance_traveled = speed_held * (race_time - speed_held);
+            if distance_traveled > race_record {
+                speeds_faster_than_record += 1;
+            }
+        }
+        sum_result_vec.push(speeds_faster_than_record);
+    }
+
+    let result = sum_result_vec
+        .into_iter()
+        .reduce(|acc, x| (acc * x))
+        .unwrap();
+
+    Solution::from(result)
 }
 
 pub fn part2(input: &str) -> Solution {
-    unimplemented!()
+    let (time_str, distance_str) = input.split_once("\n").unwrap();
+    let time = time_str
+        .split_once(": ")
+        .unwrap()
+        .1
+        .trim()
+        .replace(" ", "")
+        .parse::<usize>()
+        .unwrap();
+
+    let distance = distance_str
+        .split_once(": ")
+        .unwrap()
+        .1
+        .trim()
+        .replace(" ", "")
+        .parse::<usize>()
+        .unwrap();
+
+    let mut speeds_faster_than_record = 0;
+    for speed_held in 0..time {
+        let distance_traveled = speed_held * (time - speed_held);
+        if distance_traveled > distance {
+            speeds_faster_than_record += 1;
+        }
+    }
+
+    Solution::from(speeds_faster_than_record)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT_ONE: &str = r"";
-    const TEST_INPUT_TWO: &str = r"";
+    const TEST_INPUT_ONE: &str = r"Time:      7  15   30
+Distance:  9  40  200";
+    const TEST_INPUT_TWO: &str = TEST_INPUT_ONE;
 
     #[test]
     fn test_part_1() {
-        let fasit = Solution::from(100);
+        let fasit = Solution::from(288 as usize);
         let part_solution = part1(TEST_INPUT_ONE);
         assert_eq!(fasit, part_solution);
     }
 
     #[test]
     fn test_part_2() {
-        let fasit = Solution::from(200);
+        let fasit = Solution::from(71503);
         let my_soultion = part2(TEST_INPUT_TWO);
         assert_eq!(fasit, my_soultion);
     }
