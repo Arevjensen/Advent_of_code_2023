3dc2d21a4053af173b34a24424e6cf2accf4028f
Are Velde Jensen <areveldejensen@gmail.com>
2023-12-08 20:34:51 +0100

    day 8

--- src/day08.rs
+++ src/day08.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::io::{stdin, stdout, Read, Write};
+use std::thread::LocalKey;
+
 use crate::helpers::loader;
 use crate::helpers::solution::Solution;
 
@@ -13,31 +17,156 @@ pub fn run(part: &str) {
     println!("{:?}", result)
 }
 
+#[derive(Debug)]
+struct LocationRoutes {
+    left: String,
+    right: String,
+}
+
 pub fn part1(input: &str) -> Solution {
-    Solution::from(100)
+    let (instructions, location_data) = input.split_once("\n\n").unwrap();
+    let goal = "ZZZ".to_string();
+
+    let location_traversal = location_data
+        .lines()
+        .into_iter()
+        .map(|x| {
+            let (key, paths) = x.split_once(" = ").unwrap();
+            let binding = paths.replace("(", "").replace(")", "");
+            let (left, rigth) = binding.trim().split_once(", ").unwrap();
+            let location_routes = LocationRoutes {
+                left: left.to_string(),
+                right: rigth.to_string(),
+            };
+            (key.to_string(), location_routes)
+        })
+        .collect::<HashMap<String, LocationRoutes>>();
+
+    let mut current_location = "AAA".to_string();
+    let mut steps = 0;
+
+    for (idx, x) in instructions.chars().cycle().enumerate() {
+        if *current_location == "ZZZ".to_string() {
+            steps = idx;
+            break;
+        }
+        let current_available_routes = location_traversal.get(&current_location).unwrap();
+        current_location = match x {
+            'L' => current_available_routes.left.clone(),
+            'R' => current_available_routes.right.clone(),
+            _ => unreachable!(),
+        };
+    }
+
+    Solution::from(steps)
 }
 
 pub fn part2(input: &str) -> Solution {
-    unimplemented!()
+    let (instructions, location_data) = input.split_once("\n\n").unwrap();
+    let mut vec_start_locations = Vec::new();
+
+    let location_traversal = location_data
+        .lines()
+        .into_iter()
+        .map(|x| {
+            let (key, paths) = x.split_once(" = ").unwrap();
+            let binding = paths.replace("(", "").replace(")", "");
+            let (left, rigth) = binding.trim().split_once(", ").unwrap();
+            let location_routes = LocationRoutes {
+                left: left.to_string(),
+                right: rigth.to_string(),
+            };
+            if key.chars().last().unwrap() == 'A' {
+                vec_start_locations.push(key.to_string());
+            }
+            (key.to_string(), location_routes)
+        })
+        .collect::<HashMap<String, LocationRoutes>>();
+
+    // let mut current_location = vec_start_locations;
+    // let mut steps = 0;
+
+    let mut number_of_steps_for_each_start = HashMap::new();
+
+    for start_location in vec_start_locations {
+        let mut current_location = start_location.clone();
+        for (idx, x) in instructions.chars().cycle().enumerate() {
+            if current_location.chars().last().unwrap() == 'Z' {
+                // dbg!(&current_location);
+                number_of_steps_for_each_start.insert(start_location.clone(), idx);
+                break;
+            }
+            let current_available_routes = location_traversal.get(&current_location).unwrap();
+            current_location = match x {
+                'L' => current_available_routes.left.clone(),
+                'R' => current_available_routes.right.clone(),
+                _ => unreachable!(),
+            };
+        }
+    }
+    dbg!("started multiplying");
+
+    let steps_as_vec = Vec::from_iter(number_of_steps_for_each_start.values().map(|x| *x));
+    let result = lcm(steps_as_vec.as_slice());
+
+    Solution::Usize(result)
 }
 
+pub fn lcm(nums: &[usize]) -> usize {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
+}
+
+fn pause() {
+    let mut stdout = stdout();
+    stdout.write(b"Press Enter to continue...").unwrap();
+    stdout.flush().unwrap();
+    stdin().read(&mut [0]).unwrap();
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT_ONE: &str = r"";
-    const TEST_INPUT_TWO: &str = r"";
+    const TEST_INPUT_ONE: &str = r"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+    const TEST_INPUT_TWO: &str = r"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
 
     #[test]
     fn test_part_1() {
-        let fasit = Solution::from(100);
+        let fasit = Solution::from(2usize);
         let part_solution = part1(TEST_INPUT_ONE);
         assert_eq!(fasit, part_solution);
     }
 
     #[test]
     fn test_part_2() {
-        let fasit = Solution::from(200);
+        let fasit = Solution::from(6usize);
         let my_soultion = part2(TEST_INPUT_TWO);
         assert_eq!(fasit, my_soultion);
     }
