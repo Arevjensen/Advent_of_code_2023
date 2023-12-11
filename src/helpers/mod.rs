f031e30f0a7ea69c5b580dc1d6b7698ab266496c
Are Velde Jensen <areveldejensen@gmail.com>
2023-12-11 21:53:32 +0100

    added generic point structs to helpers

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,3 +1,4 @@
 pub mod debugging;
 pub mod loader;
 pub mod solution;
+pub mod structs;
---
+++ src/helpers/structs.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Point2D<T> {
+    x: T,
+    y: T,
+}
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Point3D<T> {
+    x: T,
+    y: T,
+    z: T,
+}
